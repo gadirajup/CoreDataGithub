569f3bc5912763aab994d4f7cd967d1d8320c33a
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-04 09:54:40 -0700

    Initial Commit

---
+++ CoreDataGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataGithub
+//
+//  Created by Prudhvi Gadiraju on 5/4/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
